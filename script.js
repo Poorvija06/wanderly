e6fbed2408f28651471b56c6702534d8315e6df2
Poorvija06 <poorvija@gmail.com>
2026-07-17 11:33:56 +0530

    Added new file and updation in style.css files

--- script.js
+++ script.js
@@ -15,4 +15,14 @@ function closePopup() {
     window.location.href = "../index.html";
 
 }
+window.onload = function(){
 
+    setTimeout(function(){
+
+        document.getElementById("loader").style.display="none";
+
+        document.getElementById("content").style.display="block";
+
+    },2000);
+
+}
